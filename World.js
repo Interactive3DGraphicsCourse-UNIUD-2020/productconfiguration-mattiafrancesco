f72bfb13542db1b978410a9c9118fda2807b63e4
Maaago <francesco.magoga@gmail.com>
2020-01-15 23:16:01 +0100

    Partial cleanup

--- World.js
+++ World.js
@@ -7,6 +7,8 @@ import {Group} from './Group.js';
 import {Shader} from './Shader.js';
 import {ShaderParams} from './ShaderParams.js';
 
+import * as MeshesNames from './MeshesNames.js';
+
 import {GUI} from './GUI.js'
 
 import * as Model from  './Model.js'
@@ -17,7 +19,11 @@ class World
 {
 	constructor()
 	{
+		//Setup scene
 		this.scene = new THREE.Scene();
+
+		this.modelGroup = new Group();
+		this.scene.add(this.modelGroup);
 
 		this.camera = new THREE.PerspectiveCamera(75, Utils.getRatio(), 0.1, 1000);	
 		this.camera.position.set(0, 0, 10);
@@ -30,176 +36,53 @@ class World
 		this.startTime = Date.now();
 		*/
 
-		//Cube Map
-		var loader = new THREE.CubeTextureLoader();
-		let cityPath = 'textures/env/city/'
-		let bridgePath = 'textures/env/Bridge2'
-		loader.setPath( cityPath );
-
-		var textureCube = loader.load( [
-			'posx.jpg', 'negx.jpg',
-			'posy.jpg', 'negy.jpg',
-			'posz.jpg', 'negz.jpg'
-		] );
-
-
-		this.scene.background = textureCube;
-		textureCube.minFilter = THREE.LinearMipMapLinearFilter;
+		this.loader = new THREE.CubeTextureLoader();
 
 		//init
-		var uniforms;
-		var fs;
-		var vs;
-		var materialExtensions;
-
-		let pathTexturesBackCover = './textures/Back_Cover/'
-		let pathGoldTexture = "Gold/TexturesCom_Paint_GoldFake_1K"
-
-		var textureParameters = {
-			material: pathGoldTexture,
-			repeatS: 1.0,
-			repeatT: 1.0,
-		}
-
-		var diffuseMap = this.loadTexture( pathTexturesBackCover + textureParameters.material + "_albedo.png" );
-		var specularMap = this.loadTexture( pathTexturesBackCover + textureParameters.material + "_metallic.png" );
-		var roughnessMap = this.loadTexture( pathTexturesBackCover + textureParameters.material + "_roughness.png" );
-
 		Model.load('./model/scene.gltf',10, (model) =>
 		{
 			try
 			{
-
 				model.rotateX(90 * Math.PI / 180);
 				model.translateZ(-10);
 				model.translateY(-2);
 
-				var meshes = model.children[0].children[0] // array di mesh
-				meshes.children.forEach(obj => {
-					console.log(obj);
-					var testColor = 0XFF0000;
+				var meshes = model.children[0].children[0].children; // array di mesh
+				this.meshes = {};
+
+				meshes.forEach(function(obj)
+				{
 					var mesh = obj.children[0];
-					switch (obj.name) {
-						case "AntennaLines": //antenne attorno al telefono
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "AppleLogo":
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "BackCameraP1": //bordo nero attorno alle fotocamere, non copre le fotocamere (ha un buco dove sono le fotocamere)
-						break
-						case "BackCamerasCover001": //si posiziona sopra le fotocamere LE COPRE
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "BackCameraBottomGreyRing", "BackCameraTopGreyRing": //gli anelli neri delle fotocamere dietro
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "BackCameraBottomLens","BackCameraTopLens": //lenti fotocamere dietro
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "BackGlass": //vetro dietro iphone
+					this.meshes[obj.name] = mesh
 
-							console.log(diffuseMap)
-							console.log(specularMap)
-							console.log(roughnessMap)
+					var testColor = 0XFF0000;
 
-							uniforms = {
-								specularMap: { type: "t", value: specularMap},
-								diffuseMap:	{ type: "t", value: diffuseMap},
-								roughnessMap:	{ type: "t", value: roughnessMap},
-								pointLightPosition:	{ type: "v3", value: new THREE.Vector3( 7.0, 7.0, 7.0 ) },
-								clight:	{ type: "v3", value: new THREE.Vector3(100,100,100) },
-								textureRepeat: { type: "v2", value: new THREE.Vector2(textureParameters.repeatS,textureParameters.repeatT) }
-							};
+					mesh.material = new THREE.MeshBasicMaterial({color: testColor});
+				}.bind(this));
 
-							vs = new Shader("back_cover_vert.glsl").getData();
-							fs = new Shader("back_cover_frag.glsl").getData();
+				this.initMeshes();
 
-							mesh.material = new THREE.ShaderMaterial({ uniforms: uniforms, vertexShader: vs, fragmentShader: fs });
-							mesh.material.needsUpdate = true;
-
-						break
-						case "Body": //tutto il bordo latere del telefono	
-
-							var materialExtensions = {
-								derivatives: true, // set to use derivatives
-								shaderTextureLOD: true // set to use shader texture LOD
-							};
-
-							var uniforms = {
-									cspec:	{ type: "v3", value: new THREE.Vector3(0.8,0.8,0.8) },
-									envMap:	{ type: "t", value: textureCube},
-									roughness: { type: "f", value: 0.2},
-								};
-			
-							var shader = new Shader("glossyRef");
-
-							this.params = new ShaderParams(shader, uniforms, materialExtensions);
-							this.params.addMesh(mesh);
-
-							break
-						case "CameraBump": //la parte in rilievo che contiene le fotocamere dietro
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "CE": //logo CE dietro
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "EarphoneGrill": //griglia microfono davanti (sopra il notch)
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "FlashBG","FlashLED": //elementi che compongono il flash (elemento quadrato in mezzo alle due fotocamere dietro)
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "Front": //corpo davanti che circonda lo schermo
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "FrontCamera": //camera frontale
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "FrontGlass": //vetro dello schermo schermo
-							break
-						case "iPhoneLogoBack":
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "MuteSwitch","PowerButton":
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "Screen": //schermo ( si trova sotto il vetro "FrontGlass")
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "Screw": //non ho capito che roba è (è piccola e inutile trascurare)		N.B. screw = viti
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "SimSlot":
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "Text : Assembled in China", "Text: Designed By Apple in California":
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-						case "VolumeButtons":
-							mesh.material = new THREE.MeshBasicMaterial({color: testColor});
-							break
-					}
-				});
-
-				this.scene.add(model);
+				this.modelGroup.add(model);
 			}
 			catch(e)
 			{
 				alert(e);
+				console.log(e);
 			}
 		},() => {});
 	}
 
 	loadTexture(file) {
-		var texture = new THREE.TextureLoader().load( file , function ( texture ) {
+		var texture = this.loader.load( file , function ( texture ) {
 
 			texture.minFilter = THREE.LinearMipMapLinearFilter;
 			// texture.anisotropy = renderer.getMaxAnisotropy();
 			texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 	  		texture.offset.set( 0, 0 );
 			texture.needsUpdate = true;
-		} )
+
+			//document.appendChild(texture.image.attributes[1].ownerElement);
+		});// , () => {}, (e) => {alert(e); console.log(e);});
 		return texture;
 	}
 
@@ -220,6 +103,86 @@ class World
 		//AmbientLight
 		var ambientLight = new THREE.AmbientLight(0x999999);
 		this.scene.add(ambientLight);
+	}
+
+	initMeshes()
+	{
+		this.initBody();
+		this.initBackGlass();
+	}
+
+	initBody()
+	{
+		var mesh = this.meshes[MeshesNames.MESH_BODY];
+		console.log(this.meshes, mesh);
+
+		//Load environment texture
+		var cityPath = 'textures/env/city/'
+		//this.loader.setPath(cityPath);
+
+		var textureCube = this.loader.load([
+			cityPath+'posx.jpg', cityPath+'negx.jpg',
+			cityPath+'posy.jpg', cityPath+'negy.jpg',
+			cityPath+'posz.jpg', cityPath+'negz.jpg'
+		]);
+
+		textureCube.minFilter = THREE.LinearMipMapLinearFilter;
+		this.scene.background = textureCube;
+
+		//Setup shaders
+		var materialExtensions = {
+			derivatives: true, // set to use derivatives
+			shaderTextureLOD: true // set to use shader texture LOD
+		};
+
+		var uniforms = {
+				cspec:	{ type: "v3", value: new THREE.Vector3(0.8,0.8,0.8) },
+				envMap:	{ type: "t", value: textureCube},
+				roughness: { type: "f", value: 0.2},
+			};
+
+		var shader = new Shader("glossyRef");
+
+		var params = new ShaderParams(shader, uniforms, materialExtensions);
+		params.addMesh(mesh);
+	}
+
+	initBackGlass()
+	{
+		var mesh = this.meshes[MeshesNames.MESH_GLASS_BACK];
+
+		//Load maps
+		var pathTexturesBackCover = "./textures/Back_Cover/";
+		var pathGoldTexture = "Gold/TexturesCom_Paint_GoldFake_1K";
+
+		var textureParameters = {
+			material: pathGoldTexture,
+			repeatS: 1.0,
+			repeatT: 1.0,
+		}
+
+		var diffuseMap = this.loadTexture(pathTexturesBackCover + textureParameters.material + "_albedo.png");
+		var specularMap = this.loadTexture(pathTexturesBackCover + textureParameters.material + "_metallic.png");
+		var roughnessMap = this.loadTexture(pathTexturesBackCover + textureParameters.material + "_roughness.png");
+
+		console.log(pathTexturesBackCover + textureParameters.material + "_albedo.png")
+		console.log(specularMap)
+		console.log(roughnessMap)
+
+		var uniforms = {
+			specularMap: { type: "t", value: specularMap},
+			diffuseMap:	{ type: "t", value: diffuseMap},
+			roughnessMap:	{ type: "t", value: roughnessMap},
+			pointLightPosition:	{ type: "v3", value: new THREE.Vector3( 7.0, 7.0, 7.0 ) },
+			clight:	{ type: "v3", value: new THREE.Vector3(100,100,100) },
+			textureRepeat: { type: "v2", value: new THREE.Vector2(textureParameters.repeatS,textureParameters.repeatT) }
+		};
+
+		//Setup shaders
+		var shader = new Shader("back_cover");
+
+		var params = new ShaderParams(shader, uniforms);
+		params.addMesh(mesh);
 	}
 
 	update()
